b823d14f750fc7db45951f0d8342ddb4db1aec17
刘娟娟 <liujjxi@qq.com>
2019-07-17 00:08:52 +0800

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,9 @@ module.exports = {
     },
     extends: ["plugin:vue/essential", "@vue/typescript"],
     rules: {
-        'semi': ['error', 'never'], // 行尾不使用分号
-        "newline-after-var": 0, //变量声明后必须空一行
+        // "quotes": [1, "single", "avoid-escape"], //引号风格
+        // 'semi': ['error', 'never'], // 行尾不使用分号
+        // "newline-after-var": 0, //变量声明后必须空一行
         "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
     },
