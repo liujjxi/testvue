c12aa90189207acf497caeca92c2fc39ac85f120
刘娟娟 <liujjxi@qq.com>
2019-07-16 23:43:45 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,8 @@ module.exports = {
     },
     extends: ["plugin:vue/essential", "@vue/typescript"],
     rules: {
+        'semi': ['error', 'never'], // 行尾不使用分号
+        "newline-after-var": 0, //变量声明后必须空一行
         "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
     },
