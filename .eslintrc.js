ecb13679d10bfab7c38429e077e4e5b3c68fa027
刘娟娟 <liujjxi@qq.com>
2019-07-17 00:41:12 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,19 @@ module.exports = {
     env: {
         node: true
     },
-    // extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
+    extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
     rules: {
-        'semi': ['error', 'never'], // 行尾不使用分号
-        "newline-after-var": ['error'], //变量声明后必须空一行
-        "quotes": ["error", "single"],
+        'prettier/prettier':[
+            'error',
+            {
+                'semi': ['error', 'never'], // 行尾不使用分号
+                "newline-after-var": ['error'], //变量声明后必须空一行
+                "quotes": ["error", "single"]            }
+        ],
         "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
     },
     parserOptions: {
-        // parser: "@typescript-eslint/parser"
+        parser: "@typescript-eslint/parser"
     }
 };
