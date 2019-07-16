67250c5e268b684ab2174d318d2a4ac7a9a24ba0
刘娟娟 <liujjxi@qq.com>
2019-07-17 00:22:50 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,9 @@ module.exports = {
     },
     extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
     rules: {
-        "quotes": [1, "single", "avoid-escape"], //引号风格
         'semi': ['error', 'never'], // 行尾不使用分号
-        "newline-after-var": 0, //变量声明后必须空一行
+        "newline-after-var": ['error'], //变量声明后必须空一行
+        "quotes": ["error", "double"],
         "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
     },
